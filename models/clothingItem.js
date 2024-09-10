7ee440a07611e052f81f1b1d402039fdf05e4ff4
Aleksandr Goldshtern <alexg020573@gmail.com>
2024-09-10 22:50:33 +0300

    Initial commit

---
+++ models/clothingItem.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+});
+
+modules.exports = mongoose.model("clothingItem", clothingItemSchema);
